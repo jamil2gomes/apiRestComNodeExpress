43095902ca33a362b0a8d4d16d11cbe371ec5002
ja1000 <jamil.lanister@hotmail.com>
2022-03-18 09:18:41 -0300

    M - finalizando api

--- api/banco/index.js
+++ api/banco/index.js
@@ -3,12 +3,12 @@ const config = require('config');
 
 //string de conexão
 const instancia = new Sequelize(
-  config.get('mysql.banco'),
-  config.get('mysql.usuario'),
-  config.get('mysql.senha'),
+  config.get('development.database'),
+  config.get('development.username'),
+  config.get('development.password'),
   {
-    host:config.get('mysql.host'),
-    dialect:'mysql'
+    host:config.get('development.host'),
+    dialect:config.get('development.dialect'),
   }
 );
 
